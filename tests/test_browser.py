cfc9267b4d88f8b101ea02fc3b569de6ca90a771
Yasuhiro Ohkura <rum.yasuhiro@gmail.com>
2023-07-26 21:25:52 +0100

    テストでローカルホストを使用

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -1,10 +1,30 @@
 import unittest
 from test.support import captured_stdout
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import threading
+import time
 from browser import *
 
 
 class TestBrowser(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # サーバーを立ち上げる
+        cls.server = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+        cls.server_thread = threading.Thread(target=cls.server.serve_forever)
+        cls.server_thread.daemon = True
+        cls.server_thread.start()
+        # サーバーが立ち上がるまで少し待つ (時間がかかる場合は適宜調整)
+        time.sleep(1)
 
+    @classmethod
+    def tearDownClass(cls):
+        # サーバーをシャットダウンする
+        cls.server.shutdown()
+        cls.server.server_close()
+        cls.server_thread.join()
+
+    
     def test_parse_url(self):
         self.assertEqual(
             parse_url("http://example.org/index.html"), ("http", "example.org", "/index.html"))
@@ -12,8 +32,8 @@ class TestBrowser(unittest.TestCase):
             parse_url("http://example.org"), ("http", "example.org", "/"))
 
     def test_request(self):
-        header, body = request("http://example.org/index.html")
-        self.assertEqual(header["content-type"], "text/html; charset=UTF-8")
+        header, body = request("http://localhost/tests/index.html")
+        self.assertEqual(body, "<body><h1>Test</h1></body>")
         
     def test_show(self):
         with captured_stdout() as stdout:
