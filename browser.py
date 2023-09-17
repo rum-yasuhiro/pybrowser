3fd7c7ee3055c5c8e685a1ccfd64507824161bf4
Yasuhiro Ohkura <rum.yasuhiro@gmail.com>
2023-09-17 15:46:07 +0100

    Browserクラスを作成

--- browser.py
+++ browser.py
@@ -1,82 +1,86 @@
 import socket
 import ssl
 
-def parse_url(url):
-    scheme, url = url.split("://", 1)
-    assert scheme in ["http", "https"], "Unknown scheme {}".format(scheme)
-
-    if "/" not in url:
-        url = url + "/"
-    host, path = url.split("/", 1)
-    return (scheme, host, "/" + path)
-
-def request(url, port=None): # テスト用にポート番号を指定できる
-    scheme, host, path = parse_url(url)
-    
-    s = socket.socket(
-        family=socket.AF_INET,
-        type=socket.SOCK_STREAM,
-        proto=socket.IPPROTO_TCP,
-    )
-    # HTTPS の場合ソケットを SSL でラップ
-    if scheme == "https":
-        ctx = ssl.create_default_context()
-        s = ctx.wrap_socket(s, server_hostname=host)
-    
-    # ポートを指定
-    if port:
+class Browser:
+    def __init__(self) -> None:
         pass
-    elif scheme == "http":
-        port = 80 
-    elif scheme == "https":
-        port = 443
     
-    s.connect((host, port))
+    def parse_url(self, url):
+        scheme, url = url.split("://", 1)
+        assert scheme in ["http", "https"], "Unknown scheme {}".format(scheme)
 
-    s.send(
-        "GET {} HTTP/1.0\r\n".format(path).encode("utf8") + "Host: {}\r\n\r\n".format(host).encode("utf8")
-    )
+        if "/" not in url:
+            url = url + "/"
+        host, path = url.split("/", 1)
+        return (scheme, host, "/" + path)
 
-    # read bits of response have already arrived
-    response = s.makefile("r", encoding="utf8", newline="\r\n")
-
-    statusline = response.readline()
-    version, status, explanation = statusline.split(" ", 2)
-    assert status == "200", "{}: {}".format(
-        status, 
-        explanation
-    )
-
-    headers = {}
-    while True:
-        line = response.readline()
-        if line == "\r\n":
-            break
-        header, value = line.split(":", 1)
-        headers[header.lower()] = value.strip()
+    def request(self, url, port=None): # テスト用にポート番号を指定できる
+        scheme, host, path = self.parse_url(url)
         
-    assert "transfer-encoding" not in headers
-    assert "content-encoding" not in headers
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP,
+        )
+        # HTTPS の場合ソケットを SSL でラップ
+        if scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=host)
+        
+        # ポートを指定
+        if port:
+            pass
+        elif scheme == "http":
+            port = 80 
+        elif scheme == "https":
+            port = 443
+        
+        s.connect((host, port))
 
-    body = response.read()
-    s.close()
-    return headers, body
+        s.send(
+            "GET {} HTTP/1.0\r\n".format(path).encode("utf8") + "Host: {}\r\n\r\n".format(host).encode("utf8")
+        )
 
-# bodyを表示
-def show(body):
-    in_angle = False
-    for c in body:
-        if c == "<":
-            in_angle = True
-        elif c == ">":
-            in_angle = False
-        elif not in_angle:
-            print(c, end="")
+        # read bits of response have already arrived
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+
+        statusline = response.readline()
+        version, status, explanation = statusline.split(" ", 2)
+        assert status == "200", "{}: {}".format(
+            status, 
+            explanation
+        )
+
+        headers = {}
+        while True:
+            line = response.readline()
+            if line == "\r\n":
+                break
+            header, value = line.split(":", 1)
+            headers[header.lower()] = value.strip()
             
-def load(url):
-    headers, body = request(url)
-    show(body)
+        assert "transfer-encoding" not in headers
+        assert "content-encoding" not in headers
+
+        body = response.read()
+        s.close()
+        return headers, body
+
+    # bodyを表示
+    def show(self, body):
+        in_angle = False
+        for c in body:
+            if c == "<":
+                in_angle = True
+            elif c == ">":
+                in_angle = False
+            elif not in_angle:
+                print(c, end="")
+                
+    def load(self, url):
+        headers, body = self.request(url)
+        self.show(body)
     
 if __name__ == "__main__":
     import sys
-    load(sys.argv[1])
+    Browser().load(sys.argv[1])
--- tests/test_browser.py
+++ tests/test_browser.py
@@ -26,15 +26,15 @@ class TestBrowser(unittest.TestCase):
 
     def test_parse_url(self):
         self.assertEqual(
-            parse_url("http://example.org/index.html"), ("http", "example.org", "/index.html"))
+            Browser().parse_url("http://example.org/index.html"), ("http", "example.org", "/index.html"))
         self.assertEqual(
-            parse_url("http://example.org"), ("http", "example.org", "/"))
+            Browser().parse_url("http://example.org"), ("http", "example.org", "/"))
 
     def test_request_with_http(self):    
-        header, body = request("http://localhost/tests/index.html", port=8000)
+        header, body = Browser().request("http://localhost/tests/index.html", port=8000)
         self.assertEqual(body, "<body><h1>Test</h1></body>")
          
     def test_show(self):
         with captured_stdout() as stdout:
-            show("<body>abcdefg</body>")
+            Browser().show("<body>abcdefg</body>")
         self.assertEqual(stdout.getvalue(), "abcdefg")
