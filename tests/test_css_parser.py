2f9783c884c25680cd449a4d94d9404361a8ad59
Yasuhiro Ohkura <rum.yasuhiro@gmail.com>
2024-01-02 15:24:52 +0000

    リファクタリング：TestCSSParser.setUp メソッドを利用し、テストケースの変数をあらかじめ用意することで冗長な記述を回避

--- tests/test_css_parser.py
+++ tests/test_css_parser.py
@@ -4,97 +4,83 @@ from css_parser import *
 
 class TestCSSParser(unittest.TestCase):
     def setUp(self):
-        self.pairs = CSSParser("")
+        self.css_parser = CSSParser("")
+        self.expected_tag = "test-selector"
+        self.expected_prop_0 = "test-prop-0"
+        self.expected_prop_1 = "test-prop-1"
+        self.expected_value_0 = "test-value-0"
+        self.expected_value_1 = "test-value-1"
+        self.expected_pair_0 = self.expected_prop_0 + " : " + self.expected_value_0
+        self.expected_pair_1 = self.expected_prop_1 + " : " + self.expected_value_1
+        self.expected_body = self.expected_pair_0 + " ; " + self.expected_pair_1 + " ; "
+        self.expected_css = self.expected_tag + "{ " + self.expected_body + " } "
 
     def test_parse(self):
-        test_tag = "test-selector"
-        test_prop0 = "test-prop0"
-        test_prop1 = "test-prop1"
-        test_value0 = "test-value0"
-        test_value1 = "test-value1"
-        test_case = (
-            test_tag
-            + "{ "
-            + test_prop0
-            + " : "
-            + test_value0
-            + " ; "
-            + test_prop1
-            + " : "
-            + test_value1
-            + " ; "
-            + " } "
-        )
-        self.pairs.s = test_case
+        self.css_parser.s = self.expected_css
 
-        rules = self.pairs.parse()
-        self.assertEqual(rules[0][0].tag, test_tag)
-        self.assertEqual(rules[0][1].get(test_prop0), test_value0)
-        self.assertEqual(rules[0][1].get(test_prop1), test_value1)
+        rules = self.css_parser.parse()
+        self.assertEqual(rules[0][0].tag, self.expected_tag)
+        self.assertEqual(rules[0][1].get(self.expected_prop_0), self.expected_value_0)
+        self.assertEqual(rules[0][1].get(self.expected_prop_1), self.expected_value_1)
 
     def test_selector(self):
-        test_tag = "test-selector"
-        self.pairs.s = test_tag
-        act = self.pairs.selector()
+        self.css_parser.s = self.expected_tag
+        act = self.css_parser.selector()
         self.assertIsInstance(act, TagSelector)
-        self.assertEqual(act.tag, test_tag)
+        self.assertEqual(act.tag, self.expected_tag)
 
     def test_body(self):
-        test = "test-prop1:test-value1;test-prop2:test-value2;"
-        self.pairs.s = test
-        act = self.pairs.body()
-        self.assertEqual(act.get("test-prop1"), "test-value1")
-        self.assertEqual(act.get("test-prop2"), "test-value2")
+        test = self.expected_body
+        self.css_parser.s = test
+        act = self.css_parser.body()
+        self.assertEqual(act.get(self.expected_prop_0), self.expected_value_0)
+        self.assertEqual(act.get(self.expected_prop_1), self.expected_value_1)
 
     def test_whitespace(self):
-        self.pairs.s = "     "
-        self.pairs.whitespace()
-        self.assertEqual(self.pairs.i, 5)
+        self.css_parser.s = "     "
+        self.css_parser.whitespace()
+        self.assertEqual(self.css_parser.i, 5)
 
     def test_word(self):
-        exp = "testWord"
-        self.pairs.s = exp
-        self.assertEqual(self.pairs.word(), exp)
-        self.assertEqual(self.pairs.i, 8)
+        self.css_parser.s = self.expected_prop_0
+        self.assertEqual(self.css_parser.word(), self.expected_prop_0)
+        self.assertEqual(self.css_parser.i, 11)
 
     def test_word_exception(self):
         wrong_input = " testWord"
-        self.pairs.s = wrong_input
+        self.css_parser.s = wrong_input
         with self.assertRaises(Exception, msg="Parsing error"):
-            self.pairs.word()
+            self.css_parser.word()
 
     def test_literal(self):
         exp = "{testWord"
-        self.pairs.s = exp
-        self.pairs.literal("{")
-        self.assertEqual(self.pairs.i, 1)
+        self.css_parser.s = exp
+        self.css_parser.literal("{")
+        self.assertEqual(self.css_parser.i, 1)
 
     def test_literal_exception(self):
         exp = "testWord"
-        self.pairs.s = exp
+        self.css_parser.s = exp
         with self.assertRaises(Exception, msg="Parsing error"):
-            self.pairs.literal("{")
+            self.css_parser.literal("{")
 
     def test_pair(self):
-        test = "test-prop:test-value"
-        exp_prop = "test-prop"
-        exp_val = "test-value"
-        self.pairs.s = test
-        prop, val = self.pairs.pair()
-        self.assertEqual(prop, exp_prop)
-        self.assertEqual(val, exp_val)
+        self.css_parser.s = self.expected_pair_0
+        prop, val = self.css_parser.pair()
+        self.assertEqual(prop, self.expected_prop_0)
+        self.assertEqual(val, self.expected_value_0)
 
     def test_ignore_until(self):
-        test = "{test-test:test}"
-        self.pairs.s = test
-        self.assertEqual(self.pairs.ignore_until(["}"]), "}")
-        self.assertEqual(self.pairs.i, len(test) - 1)
+        test_case = self.expected_css
+        self.css_parser.s = test_case
+        self.assertEqual(self.css_parser.ignore_until(["}"]), "}")
+        self.assertEqual(self.css_parser.i, len(test_case) - 2)
 
     def test_ignore_until_not_included(self):
-        test = "{test-test:test"
-        self.pairs.s = test
-        self.assertEqual(self.pairs.ignore_until(["}"]), None)
-        self.assertEqual(self.pairs.i, len(test))
+        test_case = "{" + self.expected_pair_0
+        self.css_parser.s = test_case
+        self.assertEqual(self.css_parser.ignore_until(["}"]), None)
+        self.assertEqual(self.css_parser.i, len(test_case))
 
 
 class TestTagSelector(unittest.TestCase):
